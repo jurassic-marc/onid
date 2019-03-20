0c52a50ef80f1f1b1948d663fbbd09c0f7aba352
jurassic-marc <m_templeton@hotmail.co.uk>
2019-03-20 20:07:17 +0000

    advanced logging added

--- q/bin/launch.sh
+++ q/bin/launch.sh
@@ -1,1 +1,11 @@
-pyq
+
+home_path='/home/marc/'
+usr_path='/usr/'
+
+q_path=$home_path'q/l32/q'
+rlwrap_path=$usr_path'bin/rlwrap'
+
+src_path=$home_path'git/onid/q/src/'
+
+
+$rlwrap_path -r $q_path $src_path'init.q' -c 30 2000i -p 5001 -log info

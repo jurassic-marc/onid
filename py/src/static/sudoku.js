aad8b4a32523e4c1a02f816426840b886461470d
jurassic-marc <m_templeton@hotmail.co.uk>
2019-04-08 05:30:49 +0100

    clues immutable

--- py/src/static/sudoku.js
+++ py/src/static/sudoku.js
@@ -2,8 +2,9 @@
 //https://stackoverflow.com/questions/950087/how-do-i-include-a-javascript-file-in-another-javascript-file
 $.getScript("../static/utils.js");
 
+var clueBoard = [];
+
 var app = angular.module('sudoku', ['ngSanitize']);
-test=[];
 app.controller('sudokuCtrl', function($scope, $http) {
   $scope.solveBoard = function() {
     board = cutBoard(extractBoard(), 9).map(i => {
@@ -28,14 +29,14 @@ app.controller('sudokuCtrl', function($scope, $http) {
 
   $scope.fetchPuzzle = function() {
     diff = document.getElementById("puzzle_difficulty").value;
-    console.log(diff);
     $http.get("/puzzle/" + String(diff))
       .then(function(response) {
-          var board = response.data;
-          overwriteBoard(objectifyBoard(cutBoard(board, 9),9));
+          var board = cutBoard(response.data, 9);
+          overwriteBoard(objectifyBoard(board, 9));
+          makeCluesImmutable(board, 9);
         },
         function onError(response) {
-          alert("problem with your request.");
+          alert("Sorry, there was a problem with your request.");
         }
       );
 }

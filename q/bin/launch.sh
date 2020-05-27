809914841812f3c1670cf7251b3c3830d3a7474e
jurassic-marc <m_templeton@hotmail.co.uk>
2020-05-27 16:22:16 +0100

    fix bug in launch.sh

--- q/bin/launch.sh
+++ q/bin/launch.sh
@@ -5,7 +5,7 @@ usr_path='/usr/'
 q_path=$home_path'q/l32/q'
 rlwrap_path=$usr_path'bin/rlwrap'
 
-src_path=$home_path'/path/to/repo/proj/onid/q/src/
+src_path=$home_path'/path/to/repo/proj/onid/q/src/'
 
 
 $rlwrap_path -r $q_path $src_path'init.q' -c 30 2000i -p 5001 -log info
